feda52946944a1a4f548735c0f8404f43ba82263
Rudi Persson <Rudipersson@gmail.com>
2018-05-02 12:34:30 +0200

    Basic graphQL server created.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import graphqlHTTP from "express-graphql";
+import schema from "./schema";
 
 const app = express();
 
@@ -6,4 +8,15 @@ app.get("/", (req, res) => {
   res.send("GraphQL server woks");
 });
 
-app.listen(8080, () => console.log("Server running on port localhost:8080"));
+const root = { hello: () => "Hi, I'am Rudi" };
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
+
+app.listen(8080, () => console.log("Server running on port localhost:8080. For grapqQl: localhost:8080/graphql"));
